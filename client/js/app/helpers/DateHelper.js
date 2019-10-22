50a775d837ba28b9a2f0aae8208517f33c8e8fd4
Joao <joao@maven.com.br>
2019-10-22 15:32:16 -0300

    next

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,12 +1,17 @@
 class DateHelper{
 
-    dataParaTexto(data){
+    constructor(){
+        throw new Error("DateHelper não pode ser instanciada");
+        
+    }
+
+    static dataParaTexto(data){
         return data.getDate()
         + '/' + (data.getMonth() + 1)
         + '/' + data.getFullYear();
     }
 
-    textoParaData(texto){
+    static textoParaData(texto){
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
