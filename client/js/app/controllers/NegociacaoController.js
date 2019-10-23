b0d77e36d0b96d7543a63270b61fc877eb96757a
JohnLessa <joao.mello.lessa@hotmail.com>
2019-10-22 21:43:56 -0300

    Final Aula4

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,19 +6,29 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
     
     adiciona(event) {
         
         event.preventDefault();
-            
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value  
-        );
-        
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+            this._inputValor.value);
+    }
+
+    _limpaFormulario(){
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
     }
 }
--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -6,12 +6,15 @@ class DateHelper{
     }
 
     static dataParaTexto(data){
-        return data.getDate()
-        + '/' + (data.getMonth() + 1)
-        + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
+
     }
 
     static textoParaData(texto){
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+           alert('Data deve estar no formato yyyy-mm-dd');
+           throw new Error('Data deve estar no formato yyyy-mm-dd');
+        }
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
